2868ed570526ba4e427fbf23100f909aa09ae755
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-08 10:57:35 +0100

    mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +15,30 @@ app.get("/", (req, res) => {
   res.send("Service review server is running");
 });
 
+//Database connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+//Function for Creating collection to the database
+async function run() {
+  try {
+    const serviceCollection = client.db("serviceReview").collection("services");
+    //get all data from db
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 app.listen(port, () => {
   console.log(`Service review server running on ${port}`);
 });
